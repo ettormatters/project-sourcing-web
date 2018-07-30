a1e88eb775eedfaea46b0f8792c17ba7e9cd4de3
oeritas <advanwith@gmail.com>
2018-07-30 23:50:50 +0900

    components

--- components/StageBanner.js
+++ components/StageBanner.js
@@ -4,9 +4,12 @@ class StageBanner extends React.Component {
     render(){
         return(
             <div>
-                <img src="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2018/06/07/15283977760213.jpg"/>
+                <img src="http://www.v3wall.com/wallpaper/1920_1080/1101/1920_1080_20110115093323292435.jpg"/>
                 <style jsx>{`
-                    
+                    img {
+                        width: 1300px;
+                        height: 600px;
+                    }
                 `}</style>
             </div>
         );
