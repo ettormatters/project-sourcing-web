2b297ac13664f890fa4607147c2abee16b06c5a4
oeritas <advanwith@gmail.com>
2018-08-13 18:50:53 +0900

    FB login : ok / code clear, token : proceeding 2

--- pages/login.js
+++ pages/login.js
@@ -36,22 +36,17 @@ class FBLogin extends React.Component {
 
           function checkLoginState() {
             FB.getLoginStatus(function(response) {
-                console.log(response)
                 statusChangeCallback(response);
             });
           }
 
           function statusChangeCallback(response) {
+            console.log(response)
             if (response.status === 'connected') {
-                // Logged into your app and Facebook.
-                console.log('Welcome!  Fetching your information.... ');
                 FB.api('/me', function (response) {
                     console.log('Successful login for: ' + response.name);
-                    document.getElementById('status').innerHTML =
-                      'Thanks for logging in, ' + response.name + '!';
                 });
             } else {
-                // The person is not logged into your app or we are unable to tell.
                 document.getElementById('status').innerHTML = 'Please log ' +
                   'into this app.';
             }
