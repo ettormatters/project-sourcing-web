8b2377e62831a8c19f9552cc3b580ca400ba32cd
oeritas <advanwith@gmail.com>
2018-09-03 22:38:14 +0900

    RBoot remove, FB login at index : ing

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,6 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous"/>
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossOrigin="anonymous"/>
             <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
         <body>
--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,53 @@ class Index extends React.Component {
                 }
             `
         request('http://localhost:4000/graphql', initQuery)
-            .then((res)=>{store.dispatch(initPosts(res.getInitialPosts));})
+        .then((res)=>{store.dispatch(initPosts(res.getInitialPosts));})
+        
+        const s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.innerHTML = `
+
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '650717358631482',
+                cookie     : true,
+                xfbml      : true,
+                version    : 'v3.1'
+            }); 
+            FB.AppEvents.logPageView();   
+        };
+
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1&appId=650717358631482";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+        function checkLoginState() {
+            FB.getLoginStatus(function(response) {
+                statusChangeCallback(response);
+            });
+        }
+
+        function statusChangeCallback(response) {
+            console.log(response)
+            if (response.status === 'connected') {
+                FB.api('/me', function (response) {
+                    console.log('Successful login for: ' + response.name);
+                });
+            } else {
+                document.getElementById('status').innerHTML = 'Please log ' +
+                    'into this app.';
+            }
+        }
+
+        `;
+        document.body.appendChild(s);
+        let scriptList = document.scripts;
+        console.log(scriptList)
     }
 
     shouldComponentUpdate(){
@@ -70,6 +116,8 @@ class Index extends React.Component {
                     <StageBanner />
                     <div className="container">
                         <Category selChange={this._orderSelectOnChange} />
+                            <div id="status"></div>
+                            <div className="fb-login-button" data-button-type="continue_with" data-size="medium" data-auto-logout-link="true" data-onlogin="checkLoginState();"></div>   
                         <PostView/>
                     </div>
 
